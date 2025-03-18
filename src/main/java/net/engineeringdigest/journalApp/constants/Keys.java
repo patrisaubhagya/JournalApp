a9208013f21c4d3fe6684cd54e012b48b8e904e0
Saubhagya Patri <patrisaubhagya@users.noreply.github.com>
2025-03-18 22:41:34 +0530

    Final commit

---
+++ src/main/java/net/engineeringdigest/journalApp/constants/Keys.java
@@ -0,0 +1,5 @@
+package net.engineeringdigest.journalApp;
+
+public enum Keys {
+    WEATHER_API
+}
---
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,4 @@
+package net.engineeringdigest.journalApp.scheduler;
+
+public class UserScheduler {
+}
